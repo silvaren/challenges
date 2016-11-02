6055556e105ffa87ca554c7ecd0b1a2b49b64874
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-02 13:57:42 -0200

    Converting code to Scala idiom

--- src/main/scala/AppleAndOrange.scala
+++ src/main/scala/AppleAndOrange.scala
@@ -1,13 +1,13 @@
 object AppleAndOrange {
 
   def main(args: Array[String]){
-    val sc = new java.util.Scanner (System.in);
-    var s = sc.nextInt();
-    var t = sc.nextInt();
-    var a = sc.nextInt();
-    var b = sc.nextInt();
-    var m = sc.nextInt();
-    var n = sc.nextInt();
+    val sc = new java.util.Scanner (System.in)
+    val s = sc.nextInt()
+    val t = sc.nextInt()
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+    val m = sc.nextInt()
+    val n = sc.nextInt()
     val appleDs = for (x <- 0 until m) yield sc.nextInt()
     val orangeDs = for (x <- 0 until n) yield sc.nextInt()
     println(appleDs.count(x => a + x >= s && a + x <= t))
--- src/main/scala/Kangaroo.scala
+++ src/main/scala/Kangaroo.scala
@@ -3,11 +3,11 @@ object Kangaroo {
   object Solution {
 
     def main(args: Array[String]) {
-      val sc = new java.util.Scanner (System.in);
-      var x1 = sc.nextInt();
-      var v1 = sc.nextInt();
-      var x2 = sc.nextInt();
-      var v2 = sc.nextInt();
+      val sc = new java.util.Scanner (System.in)
+      val x1 = sc.nextInt()
+      val v1 = sc.nextInt()
+      val x2 = sc.nextInt()
+      val v2 = sc.nextInt()
 
       if (v1 <= v2) println("NO")
       else println(if ((x2 - x1) % (v1 - v2) == 0) "YES" else "NO")
