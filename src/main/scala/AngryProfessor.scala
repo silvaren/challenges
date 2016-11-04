655ea405a5e262cba1e09d20fdd4859a1129a4f6
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-04 06:39:00 -0200

    Add AngryProfessor

---
+++ src/main/scala/AngryProfessor.scala
@@ -0,0 +1,22 @@
+object AngryProfessor {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+    val t = sc.nextInt()
+
+    val tests = for (i <- 0 until t) yield {
+      val n = sc.nextInt()
+      val k = sc.nextInt()
+      val as = for (j <- 0 until n) yield sc.nextInt()
+      (n, k, as)
+    }
+
+    tests.foreach{
+      case (n, k, as) => {
+        println(if (as.count(x => x <= 0) < k) "YES" else "NO")
+      }
+    }
+
+  }
+
+}
