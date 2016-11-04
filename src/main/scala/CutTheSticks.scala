ff718ad80a6b95e58bdfd0eb6d27dd685c39455d
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-04 07:06:51 -0200

    Add CutTheSticks

---
+++ src/main/scala/CutTheSticks.scala
@@ -0,0 +1,21 @@
+object CutTheSticks {
+
+  def cutSticks(as: Seq[Int]): Seq[Int] =
+    as.collect{ case x if x > as.min => x - as.min }
+
+  def cutUntilNone(as: Seq[Int]): Unit = {
+    if (!as.isEmpty) {
+      println(as.size)
+      cutUntilNone(cutSticks(as))
+    }
+  }
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+    val n = sc.nextInt()
+    val as = for (i <- 0 until n) yield sc.nextInt()
+
+    cutUntilNone(as)
+  }
+
+}
