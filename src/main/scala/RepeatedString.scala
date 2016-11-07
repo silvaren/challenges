b41f431b895a0df08da6b76b05dea95fff14def3
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-11-07 00:41:03 -0200

    Add RepeatedStrings

---
+++ src/main/scala/RepeatedString.scala
@@ -0,0 +1,13 @@
+object RepeatedString {
+
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+    val s = sc.next()
+    val n = sc.nextLong()
+
+    val fullAs = (n / s.size) * s.toList.count( c => c == 'a')
+    val remainderAs =  s.substring(0, (n % s.size).toInt).toList.count( c => c == 'a')
+    println(fullAs + remainderAs)
+  }
+
+}
