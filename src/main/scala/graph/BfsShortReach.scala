4871c383493f9e266c4dc2a4d1b906582f627d0f
Renato Oliveira <oliveirasilvaren@gmail.com>
2017-01-29 03:36:30 -0200

    Refactor graph representation to be adjacency lists

--- src/main/scala/graph/BfsShortReach.scala
+++ src/main/scala/graph/BfsShortReach.scala
@@ -4,21 +4,23 @@ object BfsShortReach {
 
   val LENGTH = 6
 
-  def bfs(adj: Array[Array[Int]], queue: Seq[(Int,Int)], shortReach: Seq[Int]): Seq[Int] = {
+  def bfs(adj: IndexedSeq[Seq[(Int,Int)]], queue: Seq[(Int,Int)], shortReach: IndexedSeq[Int]): Seq[Int] = {
     if (queue.isEmpty) shortReach
     else {
       val node = queue.head
-      val newShortReach = shortReach.updated(node._1-1, node._2)
-      val queueAddend = for (i <- 0 until adj.length if adj(node._1-1)(i) > 0)
-        yield (i + 1, node._2 + adj(node._1-1)(i))
-      val newQueue = (queue.tail ++ queueAddend).filter{case (v,_) => newShortReach(v-1) < 0}
-      bfs(adj, newQueue, newShortReach)
+      if (shortReach(node._1-1) > 0) bfs(adj, queue.tail, shortReach)
+      else {
+        val newShortReach = shortReach.updated(node._1 - 1, node._2)
+        val queueAddend = adj(node._1 - 1).map { case (v, l) => (v, node._2 + l) }
+        val newQueue = (queue.tail ++ queueAddend)
+        bfs(adj, newQueue, newShortReach)
+      }
     }
   }
 
-  def bfsProxy(adj: Array[Array[Int]], s: Int): Unit ={
+  def bfsProxy(adj: IndexedSeq[Seq[(Int,Int)]], s: Int): Unit ={
     val n = adj.length
-    val shortReach = bfs(adj, Seq((s,0)), Seq.fill(n)(-1))
+    val shortReach = bfs(adj, Seq((s,0)), IndexedSeq.fill(n)(-1))
     val shortReachNoStartNode = shortReach.take(s - 1) ++ shortReach.drop(s)
     shortReachNoStartNode.foreach(l => print(l + " "))
     println()
@@ -33,8 +35,12 @@ object BfsShortReach {
       val es = for (j <- 0 until m) yield (sc.nextInt(), sc.nextInt())
       val s = sc.nextInt()
 
-      val adj = Array.ofDim[Int](n,n)
-      es.foreach{ case (v1: Int,v2: Int) => adj(v1-1)(v2-1) = LENGTH; adj(v2-1)(v1-1) = LENGTH}
+      val adj = Array.fill[Seq[(Int, Int)]](n)(Seq[(Int,Int)]())
+      es.foreach{ case (v1: Int,v2: Int) => {
+          adj(v1 - 1) = (v2, LENGTH) +: adj(v1 - 1)
+          adj(v2 - 1) = (v1, LENGTH) +: adj(v2 - 1)
+        }
+      }
       (adj, s)
     }
     qs.foreach(q => bfsProxy(adj = q._1, s = q._2))
