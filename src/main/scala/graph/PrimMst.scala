96956d0ffc7fe80e00279b71e25ea9c2a5928205
Renato Oliveira <oliveirasilvaren@gmail.com>
2017-02-08 01:37:42 -0200

    Add primMst algorithm

--- src/main/scala/graph/PrimMst.scala
+++ src/main/scala/graph/PrimMst.scala
@@ -1,6 +1,24 @@
 package graph
 
+import scala.annotation.tailrec
+
 object PrimMst {
+
+  case class Edge(x: Int, y: Int, length: Int)
+  case class Tree(nodes: Set[Int], edges: Set[Edge])
+
+  @tailrec
+  def primMst(tree: Tree, graph: Set[Edge], n: Int): Tree = {
+    if (tree.nodes.size == n) tree
+    else {
+      val nextEdges = graph.filter(e =>
+        tree.nodes.contains(e.x) && !tree.nodes.contains(e.y) ||
+          tree.nodes.contains(e.y) && !tree.nodes.contains(e.x))
+      val smallestEdge = nextEdges.minBy(_.length)
+      val newNode = if (tree.nodes.contains(smallestEdge.x)) smallestEdge.y else smallestEdge.x
+      primMst(Tree(tree.nodes + newNode, tree.edges + smallestEdge), graph, n)
+    }
+  }
 
   def main(args: Array[String]) {
     val sc = new java.util.Scanner(System.in)
@@ -11,12 +29,14 @@ object PrimMst {
       val y = sc.nextInt()
       val r = sc.nextInt()
 
-      (x,y,r)
+      Edge(x,y,r)
     }
     val s = sc.nextInt()
 
     ms.foreach(println(_))
     println(s)
+    val mst = primMst(Tree(Set(s),Set()), ms.toSet, n)
+    println(mst)
   }
 
 }
